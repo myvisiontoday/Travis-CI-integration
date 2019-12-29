86e70aa30a0aa5c6a07fdfff90ca4a52ffb95133
User <r.simkhada@student.fontys.nl>
2019-12-29 11:38:56 +0100

    Initial commit

---
+++ src/test/java/TravisSimpleTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TravisSimpleTest {
+    @Test
+    public void simpleTest(){
+        //arrange
+        //act
+        //assert
+        Assert.assertTrue(true);
+    }
+}
